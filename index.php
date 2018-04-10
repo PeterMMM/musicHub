620bf2e1c460a0fa2b12b1083279befc2a5a36ab
PeterMMM <minmgmg98@gmail.com>
2018-04-11 00:12:41 +0630

    initial build

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+<?php
+echo "Welcome to music hub project.<br>";
+
+?>
+
+<a href="signup.php">Sign up</a>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+Sign Up new account
+
+<br><a href="index.php">Home</a>
+</body>
+</html>
