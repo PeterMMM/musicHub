753dcf8b211433210e949aa636c4d3807693157c
PeterMMM <minmgmg98@gmail.com>
2018-04-11 14:56:19 +0630

    updated the login form with resoponsive design

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <html>
-<head></head>
+<head>
+<script src="http://localhost/musichub/javascript/jquery-1.8.0.min.js"></script>
+<link  rel="stylesheet" href="http://localhost/musichub/css/bootstrap.min.css"></style>
+<link  rel="stylesheet" href="http://localhost/musichub/css/style.css"></style>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
-<?php
-echo "Welcome to music hub project.<br>";
+<div  class="blueBar">Welcome to <b> Music Hub </b> !</div>
+<div   class="loginCa" >
 
-?>
+<div class="form-group" ">
+	<h3 class="loginTitle">Login User account</h3>
+	
+<form action="" method="" role="form">
+	<table>
+	<tr  class="text"><td>E mail : </td><td><input class="form-control" type="email" placeholder="e mail"/> </td></tr>
+	<tr class="text"><td >Password : </td><td><input class="form-control" type="password" placeholder="password"</td></tr>
 
-<a href="signup.php">Sign up</a>
+	<tr><td colspan="2" style="padding:10px; padding-left:40%;"> <input class="btn btn-default btn-md" type="submit" value="Login" name="Login"/> </td></tr>
+	
+	</table>
+	</form>
+<a style="float:left;" class="text" href="signup.php">Sign up</a>
+</div>
+</div></div>
 </body>
 </html>
