c14f15f54950a74806825418e1d2593679704602
PeterMMM <minmgmg98@gmail.com>
2018-04-12 21:36:01 +0630

    set up sql login with SQL with responsive ui

--- index.php
+++ index.php
@@ -6,23 +6,69 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+<?php
+	include 'connect.php';
+	$emailErr = $pwErr = "";
+
+//get data from form
+$userEmail = $_POST["email"];
+$password = $_POST["password"];
+
+
+
+//get username and email from db
+$sql = "SELECT user_lname, user_email, user_pw FROM user_info_file WHERE user_email = '$userEmail' OR user_pw = '$password' " ;
+$result = $conn->query($sql);
+
+if($row = $result->fetch_assoc()) {
+
+        
+        if($userEmail == $row["user_email"] &&  $password == $row["user_pw"] )
+        {
+           header("Location: newsfeed.php");
+        }elseif($userEmail == $row["user_email"] &&  $password != $row["user_pw"])
+        {
+        	 $pwErr= "Your password is wrong! ";
+       }elseif($userEmail != $row["user_email"] &&  $password == $row["user_pw"])
+       {
+       	$emailErr= "Your email is wrong";
+       }
+     }else{
+		$pwErr= "Your password is wrong! ";
+$emailErr= "Your email is wrong";
+       }
+	
+$conn->close();
+?>
 <div  class="blueBar">Welcome to <b> Music Hub </b> !</div>
 <div   class="loginCa" >
 
 <div class="form-group" ">
 	<h3 class="loginTitle">Login User account</h3>
 	
-<form action="" method="" role="form">
+<form action="" method="POST" role="form">
 	<table>
-	<tr  class="text"><td>E mail : </td><td><input class="form-control" type="email" placeholder="e mail"/> </td></tr>
-	<tr class="text"><td >Password : </td><td><input class="form-control" type="password" placeholder="password"</td></tr>
+	<tr   class="text"><td class="showTxt">E mail : </td><td><input class="form-control" type="email" name="email" placeholder="e mail"/> </td><td ><?php echo "<h6 style='font-family: TimeNewRome;color:red; padding:1.5px;'>".$emailErr."</h6>"; ?></td></tr>
+	<tr class="text"><td  class="showTxt">Password : </td><td><input class="form-control" type="password" name="password" placeholder="password"</td><td><?php echo "<h6 style='font-family: TimeNewRome;color:red; padding:1.5px;'>".$pwErr."</h6>"; ?></td></tr>
 
-	<tr><td colspan="2" style="padding:10px; padding-left:40%;"> <input class="btn btn-default btn-md" type="submit" value="Login" name="Login"/> </td></tr>
+	<tr><td colspan="3" style="padding:10px; padding-left:40%;"> <input class="btn btn-default btn-md" type="submit" value="Login" name="login"/> </td></tr>
 	
 	</table>
 	</form>
 <a style="float:left;" class="text" href="signup.php">Sign up</a>
 </div>
 </div></div>
-</body>
+
+
+
+
+
+<!-- while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["user_id"]. " - Name: " . $row["user_fname"]. " " . $row["user_lname"]. ", user email - ".$row["user_email"]."  user pw - ".$row["user_pw"]."<br>";
+     }
+ -->
+ 
+ 
+ </body>
 </html>
+
